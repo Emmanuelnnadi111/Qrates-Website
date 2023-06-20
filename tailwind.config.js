d0b618ead7ab093a6d6fbff06bfbc61e074cb3ef
Emmanuelnadi111 <doneonyi920@gmail.com>
2023-06-20 15:55:09 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.html', '*.html'],
+  theme: {
+  
+    extend: {
+     
+       fontFamily: {
+      'Roboto': ['Roboto', 'sans-serif']
+    },
+      colors: {
+      darkgray: '#406663',
+      darkgrayish: '#47473c',
+    }
+  
+   
+},
+
+   
+  },
+  plugins: [],
+}
+
